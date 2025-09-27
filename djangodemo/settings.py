fac59e9fd7978e5d27a051bad2f79365d4f26212
XiaoMingAXX <3210676508@qq.com>
2025-09-27 09:48:12 +0800

    修复端口开放

--- djangodemo/settings.py
+++ djangodemo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-)%67mj4$hvwmkd#lx*!rm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,175.178.100.179').split(',')
 
 
 # Application definition
